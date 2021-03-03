ec77da3caa428f115597750e518c897bf973aeef
GodzillaLover42 <65288132+GodzillaLover42@users.noreply.github.com>
2021-03-03 20:22:29 +0100

    Created Java Classes and XML Files for the different Pages according to the Mockup. Created UI for "Add Wine". Amended the UI for the starting Screen to look more like the Mockup and the "Add Wine" screen. You can NOT access the "Add Wine" Screen in the app itself because no logic has been implemented yet but you can check it out in the Design screen.

---
+++ app/src/main/java/com/example/wineindex/AddWine.java
@@ -0,0 +1,14 @@
+package com.example.wineindex;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddWine extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_wine);
+    }
+}
---
+++ app/src/main/java/com/example/wineindex/VineyardInfo.java
@@ -0,0 +1,14 @@
+package com.example.wineindex;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VineyardInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vineyard_info);
+    }
+}
