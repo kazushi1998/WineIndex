675fe832ba77dbf142c09a82383050679cb9f9f7
danielphpmaster <31928601+danielphpmaster@users.noreply.github.com>
2021-03-14 17:32:35 +0100

    The arrow icon now brings you back to the main activity.

--- app/src/main/java/com/example/wineindex/VineyardInfo.java
+++ app/src/main/java/com/example/wineindex/VineyardInfo.java
@@ -2,14 +2,30 @@ package com.example.wineindex;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class VineyardInfo extends AppCompatActivity {
+    private ImageButton buttonBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vineyard_info);
 
+        buttonBack = findViewById(R.id.imageButton_back);
+        buttonBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivityMain();
+            }
+        });
+    }
+
+    public void openActivityMain() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
