6b38be29d5954cf39127074ac87a0b8024d46ce6
danielphpmaster <31928601+danielphpmaster@users.noreply.github.com>
2021-03-15 12:42:40 +0100

    Added menu items.

--- app/src/main/java/com/example/wineindex/MainActivity.java
+++ app/src/main/java/com/example/wineindex/MainActivity.java
@@ -6,16 +6,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 
 public class MainActivity extends AppCompatActivity {
     private ListView listview;
     private ImageButton buttonFavorites;
+    private FloatingActionButton buttonAdd;
 
     private ListView listView;
     private String vineyardName[] = {
@@ -67,6 +72,14 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        buttonAdd = findViewById(R.id.floatingActionButton);
+        buttonAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivityAddWine();
+            }
+        });
+
         listView = (ListView) findViewById(android.R.id.list);
 
         // For populating list data
@@ -82,6 +95,28 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
     public void openActivityFavorites() {
         Intent intent = new Intent(this, Favorites.class);
         startActivity(intent);
@@ -89,6 +124,11 @@ public class MainActivity extends AppCompatActivity {
 
     public void openActivityVineyardInfo() {
         Intent intent = new Intent(this, VineyardInfo.class);
+        startActivity(intent);
+    }
+
+    public void openActivityAddWine() {
+        Intent intent = new Intent(this, AddWine.class);
         startActivity(intent);
     }
 }
